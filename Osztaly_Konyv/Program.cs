492bd13f483300466153c910b761fc4e113227bc
Kerényi Róbert <kerenyirobert18@gmail.com>
2023-04-03 11:17:31 +0200

    Alap

--- Osztaly_Konyv/Program.cs
+++ Osztaly_Konyv/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,15 +12,9 @@ namespace Osztaly_Konyv
     {
         static void Main(string[] args)
         {
-            /*for (int i = 0; i < 100; i++)
-            {
-                Konyv konyv = new Konyv("1212", "121212", "dfsdasasdas", 1988, "magyar", false, 'i');
-                Console.WriteLine(i);
-            }
-            int szorzo = (int)Math.Ceiling((double)130 / 11);*/
             Konyv k = new Konyv();
-            bool? helyes = false;
-            do
+            bool helyes = false;
+            /*do
             {
 
                 Console.Write("Add meg az ISBN számot: ");
@@ -43,12 +38,28 @@ namespace Osztaly_Konyv
                 {
                     Console.WriteLine(e.Message);
                 }
-            } while (!helyes);
+            } while (!helyes);*/
 
-
-
-
-            Console.ReadKey();
+            KonyvesPolc konyvesPolc1 = new KonyvesPolc();
+            try
+            {
+                Konyv konyv1 = new Konyv("0306406152", "Szerző 1", "Cím 1", 2023, "Magyar", false, 'n');
+                Konyv konyv2 = new Konyv("0306406152", "Szerző 1", "Cím 1", 2018, "Magyar", false, 'n');
+                konyvesPolc1.konyvHozzaAdasa(konyv1);
+                konyvesPolc1.konyvHozzaAdasa(konyv2);
+            }
+            catch (ISBN_NumberLengthException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch (ISBN_NumberFormatException e) 
+            {
+                Console.WriteLine(e.Message);
+            }
+            int db = konyvesPolc1.konyvekSzama;
+            Console.WriteLine(db);
+            Console.WriteLine(konyvesPolc1.konyvekSzama);
+            Console.WriteLine(konyvesPolc1.konyvekSzamaFuggveny());
             Console.ReadKey();
         }
     }
